c0293f040bb21f1683c7e74d81f6a6fde5025c02
Symin <syminomega@outlook.com>
2026-06-23 15:52:31 +0800

    Support URP 17 water render graph

--- Scripts/Rendering/WaterSystemFeature.cs
+++ Scripts/Rendering/WaterSystemFeature.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
+#if UNITY_6000_0_OR_NEWER
+using UnityEngine.Rendering.RenderGraphModule;
+#endif
 using UnityEngine.Rendering.Universal;
 
 namespace WaterSystem
@@ -23,6 +26,13 @@ namespace WaterSystem
             private RenderTextureDescriptor m_WaterFXDescriptor;
 #else
             private RenderTargetHandle m_WaterFX = RenderTargetHandle.CameraTarget;
+#endif
+
+#if UNITY_6000_0_OR_NEWER
+            class PassData
+            {
+                public RendererListHandle rendererListHandle;
+            }
 #endif
 
             public WaterFxPass()
@@ -84,6 +94,49 @@ namespace WaterSystem
                 CommandBufferPool.Release(cmd);
             }
 
+#if UNITY_6000_0_OR_NEWER
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+                UniversalRenderingData renderingData = frameData.Get<UniversalRenderingData>();
+                UniversalLightData lightData = frameData.Get<UniversalLightData>();
+
+                RenderTextureDescriptor descriptor = cameraData.cameraTargetDescriptor;
+                descriptor.depthBufferBits = 0;
+                descriptor.depthStencilFormat = UnityEngine.Experimental.Rendering.GraphicsFormat.None;
+                descriptor.width /= 2;
+                descriptor.height /= 2;
+                descriptor.colorFormat = RenderTextureFormat.Default;
+
+                var waterFXMap = UniversalRenderer.CreateRenderGraphTexture(renderGraph, descriptor, k_WaterFXMapName, true,
+                    FilterMode.Bilinear);
+
+                using (var builder = renderGraph.AddRasterRenderPass<PassData>(k_RenderWaterFXTag, out var passData,
+                           m_WaterFX_Profile))
+                {
+                    var drawSettings = RenderingUtils.CreateDrawingSettings(m_WaterFXShaderTag, renderingData, cameraData,
+                        lightData, SortingCriteria.CommonTransparent);
+                    var rendererListParams = new RendererListParams(renderingData.cullResults, drawSettings,
+                        m_FilteringSettings);
+
+                    passData.rendererListHandle = renderGraph.CreateRendererList(rendererListParams);
+
+                    if (!passData.rendererListHandle.IsValid())
+                        return;
+
+                    builder.UseRendererList(passData.rendererListHandle);
+                    builder.SetRenderAttachment(waterFXMap, 0, AccessFlags.Write);
+                    builder.SetGlobalTextureAfterPass(waterFXMap, k_WaterFXMapId);
+
+                    builder.SetRenderFunc((PassData data, RasterGraphContext context) =>
+                    {
+                        context.cmd.ClearRenderTarget(false, true, m_ClearColor);
+                        context.cmd.DrawRendererList(data.rendererListHandle);
+                    });
+                }
+            }
+#endif
+
             public override void OnCameraCleanup(CommandBuffer cmd) 
             {
 #if !UNITY_2022_1_OR_NEWER
@@ -111,6 +164,15 @@ namespace WaterSystem
             private ProfilingSampler m_WaterCaustics_Profile = new ProfilingSampler(k_RenderWaterCausticsTag);
             public Material WaterCausticMaterial;
             private static Mesh m_mesh;
+
+#if UNITY_6000_0_OR_NEWER
+            class PassData
+            {
+                public Material material;
+                public Matrix4x4 mainLightMatrix;
+                public Matrix4x4 drawMatrix;
+            }
+#endif
 
             public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
             {
@@ -143,6 +205,46 @@ namespace WaterSystem
                 context.ExecuteCommandBuffer(cmd);
                 CommandBufferPool.Release(cmd);
             }
+
+#if UNITY_6000_0_OR_NEWER
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+            {
+                UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+                var cam = cameraData.camera;
+                if (cam.cameraType == CameraType.Preview || !WaterCausticMaterial)
+                    return;
+
+                if (!m_mesh)
+                    m_mesh = GenerateCausticsMesh(1000f);
+
+                UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+                if (!resourceData.activeColorTexture.IsValid() || !resourceData.activeDepthTexture.IsValid())
+                    return;
+
+                using (var builder = renderGraph.AddRasterRenderPass<PassData>(k_RenderWaterCausticsTag, out var passData,
+                           m_WaterCaustics_Profile))
+                {
+                    passData.material = WaterCausticMaterial;
+                    passData.mainLightMatrix = RenderSettings.sun != null
+                        ? RenderSettings.sun.transform.localToWorldMatrix
+                        : Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(-45f, 45f, 0f), Vector3.one);
+
+                    var position = cam.transform.position;
+                    position.y = 0;
+                    passData.drawMatrix = Matrix4x4.TRS(position, Quaternion.identity, Vector3.one);
+
+                    builder.SetRenderAttachment(resourceData.activeColorTexture, 0, AccessFlags.Write);
+                    builder.SetRenderAttachmentDepth(resourceData.activeDepthTexture, AccessFlags.Write);
+                    builder.AllowGlobalStateModification(true);
+
+                    builder.SetRenderFunc((PassData data, RasterGraphContext context) =>
+                    {
+                        data.material.SetMatrix("_MainLightDir", data.mainLightMatrix);
+                        context.cmd.DrawMesh(m_mesh, data.drawMatrix, data.material, 0, 0);
+                    });
+                }
+            }
+#endif
         }
 
         #endregion
